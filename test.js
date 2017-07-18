d6966880f544095cd255b71f56eeaeb95d16c3bd
Mrugesh Mohapatra <me@raisedadead.com>
2017-07-18 12:21:50 +0000

    test: update the test for the new package name

--- test.js
+++ test.js
@@ -9,7 +9,7 @@ let generator;
 
 test.beforeEach(async () => {
 	await pify(helpers.testDirectory)(path.join(__dirname, 'temp'));
-	generator = helpers.createGenerator('nm:app', ['../app'], null, {skipInstall: true});
+	generator = helpers.createGenerator('rdd-nm:app', ['../app'], null, {skipInstall: true});
 });
 
 test.serial('generates expected files', async () => {
